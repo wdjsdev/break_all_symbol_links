e7f57c2e3f52986021e59c220ac75739501435b3
williams <31516287+wdjsdev@users.noreply.github.com>
2022-11-29 16:09:52 -0600

    rebuild
    rebuild to be much more efficient and thorough.

--- Break_All_Symbol_Links_Dev.jsx
+++ Break_All_Symbol_Links_Dev.jsx
@@ -1,141 +1,164 @@
 #target Illustrator
-function breakAllSymbolLinks()
+function breakAllSymbolLinks ()
 {
 	var valid = true;
 	var scriptName = "break_all_symbol_links";
 
-	function getUtilities()
+	function getUtilities ()
 	{
-		var result = [];
-		var utilPath = "/Volumes/Customization/Library/Scripts/Script_Resources/Data/";
-		var ext = ".jsxbin"
-
-		//check for dev utilities preference file
-		var devUtilitiesPreferenceFile = File("~/Documents/script_preferences/dev_utilities.txt");
-
-		if(devUtilitiesPreferenceFile.exists)
+		//check for dev mode
+		var devUtilitiesPreferenceFile = File( "~/Documents/script_preferences/dev_utilities.txt" );
+		var devUtilPath = "~/Desktop/automation/utilities/";
+		var devUtils = [ devUtilPath + "Utilities_Container.js", devUtilPath + "Batch_Framework.js" ];
+		function readDevPref ( dp ) { dp.open( "r" ); var contents = dp.read() || ""; dp.close(); return contents; }
+		if ( readDevPref( devUtilitiesPreferenceFile ).match( /true/i ) )
 		{
-			devUtilitiesPreferenceFile.open("r");
-			var prefContents = devUtilitiesPreferenceFile.read();
-			devUtilitiesPreferenceFile.close();
-			if(prefContents === "true")
+			$.writeln( "///////\n////////\nUsing dev utilities\n///////\n////////" );
+			return devUtils;
+		}
+
+
+
+
+
+
+		var utilNames = [ "Utilities_Container" ];
+
+		//not dev mode, use network utilities
+		var OS = $.os.match( "Windows" ) ? "pc" : "mac";
+		var ad4 = ( OS == "pc" ? "//AD4/" : "/Volumes/" ) + "Customization/";
+		var drsv = ( OS == "pc" ? "O:/" : "/Volumes/CustomizationDR/" );
+		var ad4UtilsPath = ad4 + "Library/Scripts/Script_Resources/Data/";
+		var drsvUtilsPath = drsv + "Library/Scripts/Script_Resources/Data/";
+
+
+		var result = [];
+		for ( var u = 0, util; u < utilNames.length; u++ )
+		{
+			util = utilNames[ u ];
+			var ad4UtilPath = ad4UtilsPath + util + ".jsxbin";
+			var ad4UtilFile = File( ad4UtilsPath );
+			var drsvUtilPath = drsvUtilsPath + util + ".jsxbin"
+			var drsvUtilFile = File( drsvUtilPath );
+			if ( drsvUtilFile.exists )
 			{
-				utilPath = "~/Desktop/automation/utilities/";
-				ext = ".js";
+				result.push( drsvUtilPath );
+			}
+			else if ( ad4UtilFile.exists )
+			{
+				result.push( ad4UtilPath );
+			}
+			else
+			{
+				alert( "Could not find " + util + ".jsxbin\nPlease ensure you're connected to the appropriate Customization drive." );
+				valid = false;
 			}
 		}
 
-		if($.os.match("Windows"))
-		{
-			utilPath = utilPath.replace("/Volumes/","//AD4/");
-		}
-
-		result.push(utilPath + "Utilities_Container" + ext);
-		result.push(utilPath + "Batch_Framework" + ext);
-
-		if(!result.length)
-		{
-			valid = false;
-			alert("Failed to find the utilities.");
-		}
 		return result;
 
 	}
 
+
+
 	var utilities = getUtilities();
-	for(var u=0,len=utilities.length;u<len;u++)
+
+
+
+
+	for ( var u = 0, len = utilities.length; u < len && valid; u++ )
 	{
-		eval("#include \"" + utilities[u] + "\"");	
+		eval( "#include \"" + utilities[ u ] + "\"" );
 	}
 
-	if(!valid)return;
-	
-	var docRef = app.activeDocument;
-	var layers = docRef.layers;
-	var aB = docRef.artboards;
-	var swatches = docRef.swatches;
+	log.l( "Using Utilities: " + utilities );
+
+	if ( !valid ) return;
+
+	var doc = app.activeDocument;
+	var layers = doc.layers;
+	var aB = doc.artboards;
+	var swatches = doc.swatches;
 	var obj = {};
 	var arr = [];
 	var curItem;
 	var curSymbols = [];
 
-	docRef.selection = null;
+	doc.selection = null;
 
-	for(var l = 0, masterLen = layers.length;l<masterLen;l++)
+	var doc = app.activeDocument;
+	var layers = doc.layers;
+	var layArray = afc( doc, "layers" );
+
+	layArray.forEach( function ( lay )
 	{
-		breakLinksOnCurrentGarment(layers[l]);
-	}
-
-	alert("Symbol Links Successfully Broken");
-
-	function breakLinksOnCurrentGarment(parentLayer)
-	{
-
-		var ppLay = getPPLay(parentLayer);
-		if(!ppLay)
+		lay.visible = true;
+		lay.locked = false;
+		afc( lay, "layers" ).forEach( function ( subLay )
 		{
-			return;
-		}
-
-
-		for(var x=0,len = ppLay.layers.length;x<len;x++)
-		{
-			var curLay = ppLay.layers[x];
-			// for(var y=0,yLen = curLay.pageItems.length;y<yLen;y++)
-			for(var y = curLay.pageItems.length - 1; y>=0; y--)
+			subLay.visible = true;
+			subLay.locked = false;
+			if ( subLay.name.match( /usa\s*collars/i ) )
 			{
-				curItem = curLay.pageItems[y];
-				dig(curItem);
+				subLay.remove();
 			}
-			// for(var y=curSymbols.length-1;y>=0;y--)
-			// {
-			// 	curSymbols[y].group.moveToBeginning(curSymbols[y].parent);
-			// }
-			// curSymbols = [];
-			
+		} );
+	} )
+
+	function cleanupSymbolContents ( item, dest )
+	{
+		if ( item.typename.match( /pathItem/i ) && !item.filled && !item.stroked )
+		{
+			item.remove();
 		}
+		else
+		{
+			item.moveToBeginning( dest );
+		}
+
 	}
 
 
-	function dig(item)
-	{
-		if(item.typename === "SymbolItem")
-		{
-			// item.moveToBeginning(curItem);
-			var name = item.name;
-			item.breakLink();
-			if(docRef.selection.length)
-			{
-				docRef.selection[0].name = name;
-				removeHidden(docRef.selection[0]);
-				curSymbols.push({parent:curItem,group:docRef.selection[0]});
-			}
-			
-			docRef.selection = null;
-		}
-		else if(item.typename === "GroupItem")
-		{
-			for(var d=item.pageItems.length-1;d>=0;d--)
-			{
-				dig(item.pageItems[d]);
-			}
-		}
-	}
+	var symbols = afc( doc, "symbolItems" );
 
-	function removeHidden(group)
+	var tmpLay = layers.add();
+	tmpLay.name = "tmp";
+	var outputLay = layers.add();
+	outputLay.name = "output";
+
+	symbols.forEach( function ( s, index )
 	{
-		if(group.typename !== "GroupItem")
+		var destGroup = outputLay.groupItems.add();
+		destGroup.name = s.name;
+		s.duplicate( tmpLay )
+		tmpLay.symbolItems[ 0 ].breakLink();
+		ungroup( tmpLay, destGroup, 0, cleanupSymbolContents );
+		if ( afc( destGroup ).length > 0 )
 		{
-			return;
+			destGroup.move( s, ElementPlacement.PLACEAFTER );
 		}
-		for(var rh = group.pageItems.length-1;rh>=0;rh--)
+
+		s.remove();
+		afc( tmpLay ).forEach( function ( i ) { i.remove() } );
+		afc( outputLay ).forEach( function ( i ) { i.remove() } );
+	} )
+
+	tmpLay.remove();
+	outputLay.remove();
+
+	doc.selection = null;
+
+
+	layArray.forEach( function ( lay )
+	{
+		if ( lay.name.match( /bkgrd|guide/i ) )
 		{
-			if(group.pageItems[rh].hidden)
-			{
-				group.pageItems[rh].remove();
-			}
+			lay.locked = true;
 		}
-	}
-	
+	} )
+
+	alert( "All Symbol Links Broken" )
+
+
 }
 breakAllSymbolLinks();
